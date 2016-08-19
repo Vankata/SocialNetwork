27e769ac7dc27d71fdd210b38b56520430f9a359
Ivan Kolev <vancyki@abv.bg>
2016-08-19 14:37:10 +0300

    latest version, contains all updates

--- src/chat/ChatBox.java
+++
@@ -1,9 +0,0 @@
-package chat;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class ChatBox {
-
-	private Map<String, Chat> chats = new HashMap<String, Chat>();
-}
--- src/user/UserStatus.java
+++ src/user/UserStatus.java
@@ -3,24 +3,20 @@ package user;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import user.exceptions.UserStatusException;
-import wall.Post;
 
 public class UserStatus implements IUserStatus {
 	Map<String, User> allUsers = new HashMap<String, User>();
 	
 	@Override
-	public boolean containsUser(User user){
-		return false;
+	public boolean containsUser(String email){
+	return allUsers.containsKey(email);
+
 	}
 	
 	//dobavqme go v sistemata pri registraciq
-	
 	@Override
 	public void addUser(User user){
-		
+		allUsers.put(user.getEmail(), user);
 	}
 	
 	//mahame go pri banvane ili iztrivane na profila
@@ -34,4 +30,14 @@ public class UserStatus implements IUserStatus {
 	public List<User> searchUser(User user){
 		return null;
 	}
+
+	public Map<String, User> getAllUsers() {
+		return allUsers;
+	}
+
+	public void setAllUsers(Map<String, User> allUsers) {
+		this.allUsers = allUsers;
+	}
+
+	
 }
